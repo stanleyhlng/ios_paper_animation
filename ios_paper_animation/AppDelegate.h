eafc8d90c02bb01a25171ba9b7e06623ed9c01c9
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-06-17 23:16:59 -0700

    Initial Commit

---
+++ ios_paper_animation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios_paper_animation
+//
+//  Created by Stanley Ng on 6/17/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios_paper_animation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios_paper_animation
+//
+//  Created by Stanley Ng on 6/17/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
